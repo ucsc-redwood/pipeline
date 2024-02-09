9e2137c7d505310cd4b6d4f5cdacc1bfda6b14d6
Yanwen Xu <xuyanwen2012@gmail.com>
2024-02-08 17:39:10 -0800

    looks right

--- apps/gpu_demo2/main.cu
+++ apps/gpu_demo2/main.cu
@@ -8,6 +8,7 @@
 #include "cuda/common.cuh"
 #include "cuda/common/helper_cuda.hpp"
 #include "kernels/all.hpp"
+#include "types/brt.hpp"
 
 int main(const int argc, const char** argv) {
   int n = 10'000'000;
@@ -45,18 +46,51 @@ int main(const int argc, const char** argv) {
   }
 
   const auto num_unique = n;
-  const auto num_brt_nodes = num_unique - 1;
 
-  // const auto block_size = DetermineBlockSize(gpu::k_BuildRadixTree_Kernel, n);
-  // spdlog::info("block_size = {}", block_size);
+  // const auto block_size = DetermineBlockSize(gpu::k_BuildRadixTree_Kernel,
+  // n); spdlog::info("block_size = {}", block_size);
+  // const auto block_size = 256;
 
+  RadixTreeData radix_data;
+  radix_data.n_nodes = num_unique - 1;
 
-  const auto block_size = 256;
+  // clang-format off
+  checkCudaErrors(cudaMallocManaged(&radix_data.prefixN, num_unique * sizeof(uint8_t)));
+  checkCudaErrors(cudaMallocManaged(&radix_data.hasLeafLeft, num_unique * sizeof(bool)));
+  checkCudaErrors(cudaMallocManaged(&radix_data.hasLeafRight, num_unique * sizeof(bool)));
+  checkCudaErrors(cudaMallocManaged(&radix_data.leftChild, num_unique * sizeof(int)));
+  checkCudaErrors(cudaMallocManaged(&radix_data.parent, num_unique * sizeof(int)));
+  // clang-format on
 
-  
+  gpu::Dispatch_BuildRadixTree_With(num_unique,
+                                    u_sort,
+                                    radix_data.prefixN,
+                                    radix_data.hasLeafLeft,
+                                    radix_data.hasLeafRight,
+                                    radix_data.leftChild,
+                                    radix_data.parent,
+                                    my_num_blocks);
+  checkCudaErrors(cudaDeviceSynchronize());
 
+  // peek at the first 10 brt nodes
 
+  for (auto i = 0; i < 10; ++i) {
+    printf("\n");
+    printf("prefixN[%d] = %d\n", i, radix_data.prefixN[i]);
+    printf("hasLeafLeft[%d] = %d\n", i, radix_data.hasLeafLeft[i]);
+    printf("hasLeafRight[%d] = %d\n", i, radix_data.hasLeafRight[i]);
+    printf("leftChild[%d] = %d\n", i, radix_data.leftChild[i]);
+    printf("parent[%d] = %d\n", i, radix_data.parent[i]);
+  }
+
+  // ---------------------------------------------------------------------------
 
   checkCudaErrors(cudaFree(u_sort));
+  checkCudaErrors(cudaFree(radix_data.prefixN));
+  checkCudaErrors(cudaFree(radix_data.hasLeafLeft));
+  checkCudaErrors(cudaFree(radix_data.hasLeafRight));
+  checkCudaErrors(cudaFree(radix_data.leftChild));
+  checkCudaErrors(cudaFree(radix_data.parent));
+
   return 0;
 }
